3fe771f9abb8a91e98712f76becbfedd945362fc
Abraham Lemos <113347490+abrahanlp@users.noreply.github.com>
2024-11-23 20:18:26 +0100

    Basic Arduino hardware test

---
+++ Firmware/Home_Ambient_Test/Home_Ambient_Test.ino
@@ -0,0 +1,133 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BME680.h"
+#include <Adafruit_NeoPixel.h>
+
+const int rgb_pin = 8;
+const int relay_pin = 4;
+const int main_adc_pin = 0;
+const int bms_adc_pin = 1;
+const int bme_sda = 6;
+const int bme_scl = 7;
+
+const int uv_en_pin = 5;
+const int uv_adc_pin = 3;
+float uvIntensity = -1.0;
+
+Adafruit_NeoPixel rgb_led(1, rgb_pin, NEO_GRB + NEO_KHZ800);
+byte rgb_r = 0;
+byte rgb_g = 0;
+byte rgb_b = 0;
+
+TwoWire i2c = TwoWire(0);
+
+Adafruit_BME680 bme(&i2c);
+
+void setup() {
+  //Set RGB on ESP32-C3 module
+  rgb_led.begin();
+  rgb_led.setPixelColor(0, rgb_led.Color(0, 0, 0));
+
+  //Set uv measure sensor
+  pinMode(uv_en_pin, OUTPUT);
+  digitalWrite(uv_en_pin, HIGH);
+  pinMode(uv_adc_pin, INPUT);
+
+  //Set power monitor
+  pinMode(main_adc_pin, INPUT);
+  pinMode(bms_adc_pin, INPUT);
+
+  //Set relay
+  pinMode(relay_pin, OUTPUT);
+  digitalWrite(relay_pin, LOW);
+
+  //Set serial port
+  Serial.begin(115200);
+  while(!Serial);
+  Serial.println("Home Ambient ESP32 Arduino Test");
+
+  //Set BME680
+  i2c.begin(bme_sda, bme_scl, 400000);
+  if (!bme.begin()) {
+      Serial.println("BME680 sensor not found");
+  }else{
+    bme.setTemperatureOversampling(BME680_OS_8X);
+    bme.setHumidityOversampling(BME680_OS_2X);
+    bme.setPressureOversampling(BME680_OS_4X);
+    bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+    bme.setGasHeater(320, 150);
+  }
+}
+
+void loop() {
+  bme.beginReading();
+  int main_mV = analogReadMilliVolts(main_adc_pin) * 2;
+  int bms_mV = analogReadMilliVolts(bms_adc_pin) * 2;
+  
+  Serial.print("Power(mV):");   Serial.print(main_mV);    Serial.print(",");
+  Serial.print("BMS(mV):");     Serial.print(bms_mV);     Serial.print(",");
+
+  int uv_mV = analogReadMilliVolts(uv_adc_pin);
+  if(uv_mV >= 1000){
+    uvIntensity = (float)uv_mV * 0.0083 - 8.3;
+  }else{
+    uvIntensity = -1;
+  }
+
+  //Serial.print("UV(mV):");      Serial.print(uv_mV);      Serial.print(",");
+  Serial.print("UV(mW/cm2):"); Serial.print(uvIntensity);
+
+  rgb_led.setPixelColor(0, rgb_led.Color(rgb_r, rgb_g, rgb_b));
+  rgb_led.show();
+
+  if(rgb_b){
+    rgb_b += 85;
+    if(rgb_b == 255){
+      rgb_b = 0;
+    }
+  }else{
+    if(rgb_g){
+      rgb_g += 85;
+      if(rgb_g == 255){
+        rgb_g = 0;
+        rgb_b = 85;
+      }
+    }else{
+      rgb_r += 85;
+      if(rgb_r == 255){
+        rgb_r = 0;
+        rgb_g = 85;
+      }
+    }
+  }
+  
+  if(Serial.available()) {
+    byte rx_char = Serial.read();
+    switch(rx_char) {
+      case 'R':
+        digitalWrite(relay_pin, HIGH);
+        break;
+
+      default:
+        digitalWrite(relay_pin, LOW);
+        break;
+    }
+  }
+
+  delay(50);
+
+  if (bme.endReading()) {
+    Serial.print(",T(C):");     Serial.print(bme.temperature);             Serial.print(",");
+    Serial.print("P(hPa):");    Serial.print(bme.pressure / 100.0);        Serial.print(",");
+    Serial.print("H(%):");      Serial.print(bme.humidity);                Serial.print(",");
+    Serial.print("Gas(kOhm):"); Serial.print(bme.gas_resistance / 1000.0); //Serial.print(",");
+  }
+
+  //Serial.print("R:"); Serial.print(rgb_r); Serial.print(",");
+  //Serial.print("G:"); Serial.print(rgb_g); Serial.print(",");
+  //Serial.print("B:"); Serial.print(rgb_b);
+  
+  Serial.println("");
+
+  delay(450);
+}
--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # Home Ambient
  2-wire home thermostat with temperature, humidity, pressure and air quality measures.
-## Product Requirements
- - Temperature sensor.
- - Humidity sensor.
- - Air quality sensor.
- - Switcch feedback.
- - Optional display.
- - Home Assistance integration.
 
-| Requirement 	| Description 	| Definition 	|
-|:---:	|:---:	|:---:	|
-|  dfd	|  dfd	| fdd 	|
-|  	|  	|  	|
-|  	|  	|  	|
-|  	|  	|  	|
-|  	|  	|  	|
+## Power Requirements
+
+
+## Product Requirements
+
+| Requirement   | Description  | Definition |
+|:-------------:|:-------------|:----------:|
+| Temperature	|  Temperature measurement, with minimal interference from the device.	| ±2°C 	|
+| Humidity	| Humidity measurement from ambient 	| ±5% 	|
+| Air quality	| Air quality estimation by particle detection| No yet defined |
+| Switch	| 2 pole switch	| >10A	|
+| Switch feedback	| Switch feedback to check operation 	| Digital or analog	|
+| HA | Home Assistance integration	| Not yet defined	|
+| Display | Optional e-Ink display	| Not yet defined	|
+| Autonomy | Capable to operate without power	| >12h	|
