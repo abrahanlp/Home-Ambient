ada619846567f47aa6ba63b7f90a6e767e766f92
Abraham Lemos <113347490+abrahanlp@users.noreply.github.com>
2026-01-28 12:04:34 +0100

    RGB can be turned off by Serial.

--- Firmware/Home_Ambient_Test/Home_Ambient_Test.ino
+++ Firmware/Home_Ambient_Test/Home_Ambient_Test.ino
@@ -11,6 +11,7 @@ const int bme_sda = 6;
 const int bme_scl = 7;
 
 Adafruit_NeoPixel rgb_led(1, rgb_pin, NEO_GRB + NEO_KHZ800);
+bool rgb_on = false;
 byte rgb_r = 0;
 byte rgb_g = 0;
 byte rgb_b = 0;
@@ -23,7 +24,7 @@ void setup() {
   //Set RGB on ESP32-C3 module
   rgb_led.begin();
   rgb_led.setPixelColor(0, rgb_led.Color(0, 0, 0));
-  rgb_led.setBrightness(127);
+  rgb_led.setBrightness(63);
 
   //Set power monitor
   pinMode(temp_adc_pin, INPUT);
@@ -55,30 +56,29 @@ void loop() {
   bme.beginReading();
   int temp_mV = analogReadMilliVolts(temp_adc_pin);
   int sys_mV = analogReadMilliVolts(sys_adc_pin);
-  
-  Serial.print("Temp(mV):");   Serial.print(temp_mV);    Serial.print(",");
-  Serial.print("Sys(mV):");     Serial.print(sys_mV);     Serial.print(",");
 
-  rgb_led.setPixelColor(0, rgb_led.Color(rgb_r, rgb_g, rgb_b));
-  rgb_led.show();
+  if(rgb_on){
+    rgb_led.setPixelColor(0, rgb_led.Color(rgb_r, rgb_g, rgb_b));
+    rgb_led.show();
 
-  if(rgb_b){
-    rgb_b += 85;
-    if(rgb_b == 255){
-      rgb_b = 0;
-    }
-  }else{
-    if(rgb_g){
-      rgb_g += 85;
-      if(rgb_g == 255){
-        rgb_g = 0;
-        rgb_b = 85;
+    if(rgb_b){
+      rgb_b += 85;
+      if(rgb_b == 255){
+        rgb_b = 0;
       }
     }else{
-      rgb_r += 85;
-      if(rgb_r == 255){
-        rgb_r = 0;
-        rgb_g = 85;
+      if(rgb_g){
+        rgb_g += 85;
+        if(rgb_g == 255){
+          rgb_g = 0;
+          rgb_b = 85;
+        }
+      }else{
+        rgb_r += 85;
+        if(rgb_r == 255){
+          rgb_r = 0;
+          rgb_g = 85;
+        }
       }
     }
   }
@@ -87,16 +87,31 @@ void loop() {
     byte rx_char = Serial.read();
     switch(rx_char) {
       case 'R':
+      case 'r':
         digitalWrite(relay_pin, HIGH);
+        Serial.println("Relay ON");
         break;
-
+      case 'L':
+      case 'l':
+        rgb_led.setPixelColor(0, rgb_led.Color(0, 0, 0));
+        rgb_led.show();
+        if(rgb_on == false){
+          rgb_on = true;
+        }else{
+          rgb_on = false;
+        }
+        break;
       default:
+        Serial.print(rx_char);
         digitalWrite(relay_pin, LOW);
         break;
     }
   }
 
   delay(50);
+
+  Serial.print("Temp(mV):");   Serial.print(temp_mV);    Serial.print(",");
+  Serial.print("Sys(mV):");     Serial.print(sys_mV);     Serial.print(",");
 
   if (bme.endReading()) {
     Serial.print("T2(C):");     Serial.print(bme.temperature);             Serial.print(",");
